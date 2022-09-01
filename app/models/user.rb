47d0cdde781881cb738bb15a2c0a572690c161d2
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-02 02:06:54 +0300

    Add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
+  has_many :shipping_addresses, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :product_carts, dependent: :destroy
+  has_many :products, through: :product_carts
+  has_many :order_details, through: :orders
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
