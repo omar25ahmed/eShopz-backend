91547d82dc8ba0a33dbecb3eed4c5a0d02976162
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-02 02:07:08 +0300

    add shipping address routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: 'json' } do
       resources :products, only: [:index, :show, :create, :update, :destroy]
       resources :categories, only: [:index, :show, :create, :update, :destroy]
+      resources :shipping_addresses, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
