5ed8183355f3d1b23e2bd8d21c9cbe1a87cf1192
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-03 02:35:21 +0300

    render the user with image

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,6 +1,8 @@
 class CurrentUserController < ApplicationController
   before_action :authenticate_user!
   def index
-    render json: current_user, status: :ok
+    render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
+    # render json: current_user, include: [url_for(:image)], status: :ok
   end
+
 end
