f48e26c91e8e775c7970f261cfdcb40b843105e0
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-03 02:36:56 +0300

    set up aws

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# user1 = User.create(first_name: "Luka", last_name: "Kovač", email: "Luka@gmail.com", password: "123456", country: "argentina", phone: 123456, admin: true)
+# user1.image.attach(io: File.open("app/assets/images/me.webp"), filename: "me.webp")
+
+# user2 = User.create(first_name: "Lukas", last_name: "Kovačiz", email: "Lukas@gmail.com", password: "123456", country: "argentina", phone: 123456, admin: true)
+# user2.image.attach(io: File.open("app/assets/images/user1.png"), filename: "user1.png")
