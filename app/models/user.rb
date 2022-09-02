fbce5b26be0f97003b554214830e129f3f655566
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-03 02:36:40 +0300

    show the image in the model and serializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,23 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
+  include Rails.application.routes.url_helpers
   has_many :shipping_addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :product_carts, dependent: :destroy
   has_many :products, through: :product_carts
   has_many :order_details, through: :orders
+  has_one_attached :image
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  # def image_url
+  #   Rails.application.routes.url_helpers.rails_blob_path(image, only_url: true) if image.attached?
+  # end
+
+  def image_url
+    image.url if image.attached?
+  end
 end
