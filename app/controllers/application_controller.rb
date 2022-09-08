23f68f34e8d168bf5b7a5853bf1f19722f3d0549
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-09 02:59:01 +0300

    change the user parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :country, :phone, :image) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password) }
+  end
 end
