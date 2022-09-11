6dec366036d2129624a8a1991fe7d173ddc95ed3
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-11 04:25:13 +0300

    configure cancan

---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    can :read, Product
+    can :read, Category
+    return unless user.present?
+    can :read, Order, user_id: user.id
+    can :read, OrderDetail, user_id: user.id
+    can :manage, User, id: user.id
+    can :manage, ShippingAddress, user_id: user.id
+    can :manage, ProductCart, user_id: user.id
+    return unless user.admin?
+    can :manage, :all
+  end
+end
