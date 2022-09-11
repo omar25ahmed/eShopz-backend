a5b571a9f72d1a535e9b99018c4eab953d8ed380
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-11 04:25:51 +0300

    add admin attribute while fetching

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :created_at, :first_name, :last_name, :country, :phone, :image_url
+  attributes :id, :email, :created_at, :first_name, :last_name, :country, :phone, :image_url, :admin
 end
