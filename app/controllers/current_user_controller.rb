03097be0d6d2e6dc700b59ac106fba610cf7549f
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-11 04:20:19 +0300

    add update method to current user

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,7 +1,21 @@
 class CurrentUserController < ApplicationController
   before_action :authenticate_user!
+  skip_load_and_authorize_resource
   def index
     render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
-    # render json: current_user, include: [url_for(:image)], status: :ok
+  end
+
+  def update
+    if current_user.update(user_params)
+      render json: UserSerializer.new(current_user).serializable_hash[:data][:attributes], status: :ok
+    else
+      render json: current_user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :country, :phone, :image)
   end
 end
