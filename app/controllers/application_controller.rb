bb75b5abf7cf4fda891996a88e882b7adcd5333a
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-11 04:19:29 +0300

    Add cancan to the main controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include RackSessionFix
+  load_and_authorize_resource unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,5 +10,9 @@ class ApplicationController < ActionController::API
       u.permit(:first_name, :last_name, :email, :password, :country, :phone, :image)
     end
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password) }
+  end
+
+  rescue_from CanCan::AccessDenied do
+    render json: { error: 'You are not authorized to access this page.' }, status: :unauthorized
   end
 end
