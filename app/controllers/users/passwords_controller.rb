7593bf6fba1deab70d48e54a806def5c5b57666f
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-09-15 00:18:32 -0300

    Fix Linters

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Users::PasswordsController < Devise::PasswordsController
   include RackSessionFix
   # GET /resource/password/new
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
@@ -8,12 +6,14 @@ class Ability
     can :read, Product
     can :read, Category
     return unless user.present?
+
     can :read, Order, user_id: user.id
     can :read, OrderDetail, user_id: user.id
     can :manage, User, id: user.id
     can :manage, ShippingAddress, user_id: user.id
     can :manage, ProductCart, user_id: user.id
     return unless user.admin?
+
     can :manage, :all
   end
 end
