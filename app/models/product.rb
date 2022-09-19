6a403cc28d41f8acc339d5a827b291c5283f1f55
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-09-19 15:57:25 -0300

    Add stripe ids to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   has_many :order_details, dependent: :nullify
   has_many :orders, through: :order_details
   has_many_attached :images
+  after_create :create_stripe_product
 
   validates :name, :price, :description, :stock, :name_es, :description_es, :brand, presence: true
   validates :name, :name_es, length: { maximum: 200, minimum: 10 }
@@ -13,4 +14,9 @@ class Product < ApplicationRecord
   validates :stock, numericality: { greater_than_or_equal_to: 0 }
   validates :brand, length: { maximum: 200, minimum: 2 }
   # validates :images, attached: true, content_type: ['image/png', 'image/jpg', 'image/jpeg']
+
+  def create_stripe_product
+    product = Stripe::Product.create(name:, description:, default_price_data: { currency: 'usd', unit_amount_decimal: (price * 100).to_i })
+    update(stripe_product_id: product.id, stripe_price_id: product.default_price)
+  end
 end
