3346c82dae4748482ada22b427f59dce46356d87
Omar Ragheb <ahmed.raph29@gmail.com>
2022-09-19 20:50:06 +0300

    return stripe customer id

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :email, :created_at, :first_name, :last_name, :country, :phone, :image_url, :admin
+  attributes :id, :email, :created_at, :first_name, :last_name, :country, :phone, :image_url, :admin,
+             :stripe_customer_id
 end
